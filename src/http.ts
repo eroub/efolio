96d44771e261608838772428effff494b868bd66
Evan Roubekas <evan@roubekas.com>
2023-09-15 16:17:18 -0600

    frontened rework, added api for rates, calculated risk

---
+++ src/http.ts
@@ -0,0 +1,14 @@
+// http.ts
+import axios from 'axios';
+
+const port = process.env.REACT_APP_APIPORT || 3001;
+const url = process.env.REACT_APP_APIURL || `http://localhost:${port}`;
+
+const http = axios.create({
+  baseURL: url,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export default http;
